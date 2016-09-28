f68bcb5481883a4f58a0dc1e2d458386094de9d2
Hackbright Student <info@hackbrightacademy.com>
2016-09-28 12:38:27 -0700

    Changed output to correct arithmetic form.

---
+++ arithmetic.py
@@ -0,0 +1,25 @@
+import math
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def square(num1):
+    return math.sqrt(num1)
+
+def cube(num1):
+    return (num1**3)
+
+def power(num1, num2):
+    return math.pow(num1, num2)
+
+def mod(num1, num2):
+    return num1 % num2
